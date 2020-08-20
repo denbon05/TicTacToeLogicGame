0c08e9d387945da11a1b78ffbbdc7d1ffca3b9b4
den <denisamigous@gmail.com>
2020-08-20 11:59:40 +0200

    first commit

---
+++ strategies/Easy.js
@@ -0,0 +1,17 @@
+// @ts-check
+/* eslint-disable class-methods-use-this */
+
+class Easy {
+    aiStep(field) {
+      return field.reduce((acc, row, i) => {
+        const j = row.indexOf(null);
+        if (acc.length === 0 && j !== -1) {
+          return [i, j];
+        }
+        return acc;
+      }, []);
+    }
+  }
+  
+  export default Easy;
+  
